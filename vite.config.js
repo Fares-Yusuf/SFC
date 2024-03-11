295480b5fc60e756fb8efc138f8009660bdc8aa4
fares-yusuf <fyusuf.tech@gmail.com>
2024-03-11 21:18:55 +0000

    refactored the vite.config.js file

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue2 from "@vitejs/plugin-vue2";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/SFC-Dist/",
+    base: "/SFC/",
     plugins: [
         vue2(),
         legacy({
